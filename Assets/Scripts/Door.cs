4b33cd562d0c43f01222842186793d62479e016e
alper-dipci <dipcialper35@gmail.com>
2024-01-26 23:46:26 +0300

    doorAlper

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Door : MonoBehaviour, Interactable
 {
+    private bool isOpen=false;
     public void interact()
     {
+        if (!isOpen)
+        {
+            transform.DORotate(new Vector3(0, -90, 0), 1f);
+            isOpen=true;
+        }
+        else
+        {
+            transform.DORotate(new Vector3(0, 0, 0), 1f);
+            isOpen = false;
+        }
+           
         Debug.Log("interact");
     }
 }
