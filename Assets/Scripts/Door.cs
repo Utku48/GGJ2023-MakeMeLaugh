188dd8800bcb332d716d744739a80ab816eedaf0
alper-dipci <dipcialper35@gmail.com>
2024-01-27 00:35:00 +0300

    alperLast

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,18 +6,26 @@ using DG.Tweening;
 public class Door : MonoBehaviour, Interactable
 {
     private bool isOpen=false;
+    public bool isTrapDoor;
+    [SerializeField] private GameObject TrapWall;
     public void interact()
     {
+        Sequence mySequence = DOTween.Sequence();
         if (!isOpen)
         {
-            transform.DORotate(new Vector3(0, -90, 0), 1f);
+            mySequence.Append(transform.DORotate(new Vector3(0, 90, 0), 1f));
             isOpen=true;
+            if (isTrapDoor)
+            {
+                mySequence.Append(TrapWall.transform.DOLocalMove(new Vector3(TrapWall.transform.localPosition.x, TrapWall.transform.localPosition.y, TrapWall.transform.localPosition.z-2), .5f));
+            }
         }
         else
         {
-            transform.DORotate(new Vector3(0, 0, 0), 1f);
+            mySequence.Append(transform.DORotate(new Vector3(0, 0, 0), 1f));
             isOpen = false;
         }
+        mySequence.Play();
            
         Debug.Log("interact");
     }
