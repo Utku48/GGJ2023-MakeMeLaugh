3ff4bf83127d44fa2bee775ca7a36855d66acbe0
Utku48 <109678402+Utku48@users.noreply.github.com>
2024-01-27 17:34:15 +0300

    Lab

--- Assets/Scripts/SupriseBox.cs
+++ Assets/Scripts/SupriseBox.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 
 public class SupriseBox : MonoBehaviour, Interactable
 {
-    [SerializeField] private AudioSource _flyBox;
+    [SerializeField] private ParticleSystem _dust;
     public void interact()
     {
         gameObject.transform.DOMove(new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), 5f);
-        _flyBox.Play();
+
+        if (gameObject.CompareTag("x"))
+        {
+            StartCoroutine(dustPlay());
+        }
+
 
     }
+
+    IEnumerator dustPlay()
+    {
+        yield return new WaitForSeconds(3f);
+
+        _dust.Play();
+    }
+
+
 }
