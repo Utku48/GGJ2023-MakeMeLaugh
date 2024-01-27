91dd58043bca187e663684e076596591177cf4fc
alper-dipci <dipcialper35@gmail.com>
2024-01-28 02:39:21 +0300

    asd

---
+++ Assets/Scripts/VideoPlayManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoPlayManager : MonoBehaviour
+{
+    [SerializeField] GameObject IntroText;
+    private float timer =9f;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+            IntroText.SetActive(true);
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            SceneManager.LoadScene(1);
+        }
+        
+    }
+}
